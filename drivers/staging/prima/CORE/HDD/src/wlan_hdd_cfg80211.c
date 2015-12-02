f714d7e955e4170ed949d997f393ced1763fc77f
Naresh Jayaram <c_njayar@qti.qualcomm.com>
2015-12-02 11:10:37 -0600

    PRIMA: TDLS: Optimize TDLS External Control operation handling.
    Commit "Initiate the TDLS setup only when externally configured"
    defined an additional data structure to represent a force peer.
    Optimize this implementation by defining a new field isForcedPeer,
    signifying a forced peer, in TDLS peer data structure (hddTdlsPeer_t).
    
    CRs-fixed: 537537
    
    Change-Id: Ibf55499fc648868e04a2ec9f18f5dce94fe61108
    [ljc: Backport from master]
    Signed-off-by: Luis Cruz <ljc2491@gmail.com>

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg80211.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -7740,8 +7740,6 @@ static int wlan_hdd_cfg80211_tdls_oper(struct wiphy *wiphy, struct net_device *d
                     return -ENOTSUPP;
                 }
 
-                if ( 0 != wlan_hdd_tdls_remove_force_peer(pAdapter, peer) )
-                    return -EINVAL;
 
                 pTdlsPeer = wlan_hdd_tdls_find_peer(pAdapter, peer);
 
@@ -7749,11 +7747,15 @@ static int wlan_hdd_cfg80211_tdls_oper(struct wiphy *wiphy, struct net_device *d
                     hddLog(VOS_TRACE_LEVEL_INFO, "%s: " MAC_ADDRESS_STR
                            " (oper %d) peer not exsting",
                            __func__, MAC_ADDR_ARRAY(peer));
+                    return -EINVAL;
                 }
                 else {
                     wlan_hdd_tdls_indicate_teardown(pAdapter, pTdlsPeer,
                                        eSIR_MAC_TDLS_TEARDOWN_UNSPEC_REASON);
                 }
+
+                if ( 0 != wlan_hdd_tdls_set_force_peer(pAdapter, peer, FALSE) )
+                    return -EINVAL;
                 break;
             }
         case NL80211_TDLS_SETUP:
@@ -7772,13 +7774,14 @@ static int wlan_hdd_cfg80211_tdls_oper(struct wiphy *wiphy, struct net_device *d
                     return -ENOTSUPP;
                 }
 
-                if ( 0 != wlan_hdd_tdls_add_force_peer(pAdapter, peer) )
-                    return -EINVAL;
                 /* To cater the requirement of establishing the TDLS link
                  * irrespective of the data traffic , get an entry of TDLS peer.
                  */
                 pTdlsPeer = wlan_hdd_tdls_get_peer(pAdapter, peer);
                 if (pTdlsPeer == NULL)
+                    return -EINVAL;
+
+                if ( 0 != wlan_hdd_tdls_set_force_peer(pAdapter, peer, TRUE) )
                     return -EINVAL;
                 break;
             }
